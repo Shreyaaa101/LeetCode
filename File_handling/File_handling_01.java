0a2a0321afc9c7f277be8096eb8eb3efad25d03d
Shreya <shreyathapliyal29@gmail.com>
2025-08-29 19:20:01 +0530

    added canWrite() and File library

--- File_handling/File_handling_01.java
+++ File_handling/File_handling_01.java
@@ -1,18 +1,19 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-
+import java.io.File;
 class File_handling_01{
     public static void main(String args[])
     {
         try{
-        FileWriter obj= new FileWriter("file.txt",true);
+        File f= new File("text.txt");
+        FileWriter obj= new FileWriter(f,true);
         obj.write("Working with files is EZZZZZZZZ \n");
+        System.out.println(f.canWrite());
         obj.close();
         }
         catch(IOException e){
             System.out.println(e);
         }
-
     }
 }
