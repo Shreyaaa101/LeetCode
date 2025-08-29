cf24af1db59b6baebb524dcdd57e4c4e51741b48
Shreya <shreyathapliyal29@gmail.com>
2025-08-29 21:01:59 +0530

    File Reading and Writing

--- File_handling/File_handling_01.java
+++ File_handling/File_handling_01.java
@@ -1,19 +1,43 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.io.File;
+
 class File_handling_01{
+    void write_File(){
+        try{
+            File f= new File("text.txt");
+            FileWriter obj= new FileWriter(f,true);
+            obj.write("Working with files is EZZZZZZZZ Amazing\n");
+            System.out.println(f.canWrite());
+            obj.close();
+            }
+            catch(IOException e){
+                System.out.println(e);
+            }
+}
+    void read_file(){
+        try{
+            File f= new File("text.txt");
+            Scanner sc= new Scanner(f);
+            while(sc.hasNextLine()){
+                String data=sc.nextLine();
+                System.out.println(data);
+                
+        } 
+         sc.close();
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+       
+            }
     public static void main(String args[])
     {
-        try{
-        File f= new File("text.txt");
-        FileWriter obj= new FileWriter(f,true);
-        obj.write("Working with files is EZZZZZZZZ \n");
-        System.out.println(f.canWrite());
-        obj.close();
-        }
-        catch(IOException e){
-            System.out.println(e);
-        }
+        File_handling_01 ob= new File_handling_01();
+        ob.write_File();
+        ob.read_file();
     }
-}
+    }
